473349325ba170d37c7e4dca74803516068a2da1
LuzPaty <luzpatyhr@gmail.com>
2024-07-30 01:07:57 -0600

    padding y text size

--- proactibleNewVersion/src/pages/Inicio/home_components/Nosotros.jsx
+++ proactibleNewVersion/src/pages/Inicio/home_components/Nosotros.jsx
@@ -4,11 +4,11 @@ export default function Nosotros() {
   return (
     <div className="flex flex-col items-center justify-center space-y-4 p-1">
       <div className="text-center">
-        <h1 className="text-5xl sm:text-3xl md:text-4xl lg:text-5xl font-normal text-blue pb-2 font-serif">
+        <h1 className="text-4xl md:text-6xl font-normal text-blue p-2 font-serif">
           Conoce nuestros servicios
         </h1>
       </div>
-      <div className="p-5 px-20">
+      <div className="px-4 sm:px-6 md:px-10 lg:px-20">
         <p className="text-xl sm:text-lg md:text-xl lg:text-2xl font-normal text-darkGray font-serif text-center">
           Nos enfocamos en la atención integral de las personas con una amputación de miembro inferior
           (pierna) para que la recuperación de su movilidad sea económicamente accesible.
