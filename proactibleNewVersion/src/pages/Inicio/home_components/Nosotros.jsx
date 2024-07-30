38a836cef82c070b55526030f37d9115f21b937b
LuzPaty <luzpatyhr@gmail.com>
2024-07-30 00:46:57 -0600

    nosotros acomodado

--- proactibleNewVersion/src/pages/Inicio/home_components/Nosotros.jsx
+++ proactibleNewVersion/src/pages/Inicio/home_components/Nosotros.jsx
@@ -2,19 +2,17 @@ import React from "react";
 
 export default function Nosotros() {
   return (
-    <div className="flex flex-col items-center justify-center space-y-4">
-      <div>
-
-      <h1 className="text-5xl font-normal leading-[px] text-blue pb-[10px] font-serif break-all">
-        Conoce nuestros servicios 
-      </h1>
+    <div className="bg-blue-700 flex flex-col items-center justify-center space-y-4 p-1">
+      <div className="bg-green-400 text-center">
+        <h1 className="text-5xl sm:text-3xl md:text-4xl lg:text-5xl font-normal text-blue pb-2 font-serif">
+          Conoce nuestros servicios
+        </h1>
       </div>
-      <div className="p-10 px-20">
-
-      <p className="text-2xl font-normal text-darkGray font-serif text-center">
-      Nos enfocamos en la atención integral de las personas con una amputación de miembro inferior
-      (pierna) para que la recuperación de su movilidad sea económicamente accesible.
-      </p>
+      <div className="bg-orange-400 p-5 px-20">
+        <p className="text-3xl md:text-xl font-normal text-darkGray font-serif text-center">
+          Nos enfocamos en la atención integral de las personas con una amputación de miembro inferior
+          (pierna) para que la recuperación de su movilidad sea económicamente accesible.
+        </p>
       </div>
     </div>
   );
