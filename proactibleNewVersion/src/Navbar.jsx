b446cfeac99874bc123401cd901bb339de3c80d2
LuzPaty <luzpatyhr@gmail.com>
2024-08-14 00:47:48 -0600

    correcciones navbar

--- proactibleNewVersion/src/Navbar.jsx
+++ proactibleNewVersion/src/Navbar.jsx
@@ -19,58 +19,66 @@ export default function Navbar() {
   return (
     <nav className="bg-white shadow-lg p-4">
       <div className="container mx-auto flex items-center justify-between">
+        
         {/* Logo on the left */}
-        <div className="flex items-center">
+        <div className="flex items-center flex-none">
           <Link to="/" className="flex items-center">
             <img src={logo1} alt="logo" className="w-32 h-auto" />
           </Link>
         </div>
 
-        {/* Centered navigation items on larger screens */}
-        <ul className="hidden sm:flex items-center justify-center flex-1 space-x-6">
-          <li className="list-none">
-            <Link
-              to="/"
-              className="flex items-center text-gray-800 hover:text-blue-600 transition-colors duration-200"
-            >
-              {homeIcon}
-              <span className="ml-2 text-base sm:text-lg">Inicio</span>
-            </Link>
-          </li>
-          <li className="list-none">
-            <Link
-              to="/nosotros"
-              className="flex items-center text-gray-800 hover:text-blue-600 transition-colors duration-200"
-            >
-              {aboutIcon}
-              <span className="ml-2 text-base sm:text-lg">Nosotros</span>
-            </Link>
-          </li>
-          <li className="list-none">
-            <Link
-              to="/contacto"
-              className="flex items-center text-gray-800 hover:text-blue-600 transition-colors duration-200"
-            >
-              {contactIcon}
-              <span className="ml-2 text-base sm:text-lg">Contacto</span>
-            </Link>
-          </li>
-          <li className="list-none">
-            <a
-              href={WHATSAPP_LINK}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center text-gray-800 hover:text-blue-600 transition-colors duration-200"
-            >
-              {whatsappIcon}
-              <span className="ml-2 text-base sm:text-lg">Whatsapp</span>
-            </a>
-          </li>
-        </ul>
+        {/* Centered navigation items, hidden on mobile */}
+        <div className="flex-1  justify-center sm:flex hidden">
+          <ul className="flex items-center space-x-6">
+            <li className="list-none">
+              <Link
+                to="/"
+                className="flex items-center text-customBlue hover:text-blue-600 transition-colors duration-200"
+              >
+                {homeIcon}
+                <span className="ml-2 text-base">Inicio</span>
+              </Link>
+            </li>
+            <li className="list-none">
+              <Link
+                to="/nosotros"
+                className="flex items-center text-customBlue hover:text-blue-600 transition-colors duration-200"
+              >
+                {aboutIcon}
+                <span className="ml-2 text-base">Nosotros</span>
+              </Link>
+            </li>
+            <li className="list-none">
+              <Link
+                to="/contacto"
+                className="flex items-center text-customBlue hover:text-blue-600 transition-colors duration-200"
+              >
+                {contactIcon}
+                <span className="ml-2 text-base">Contacto</span>
+              </Link>
+            </li>
+            <li className="list-none">
+              <a
+                href={WHATSAPP_LINK}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center text-customBlue hover:text-blue-600 transition-colors duration-200"
+              >
+                {whatsappIcon}
+                <span className="ml-2 text-base">Whatsapp</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        {/* Empty column to help with distribution */}
+        <div className="flex-none w-32">
+          {/* This div is empty to help with distribution */}
+        </div>
 
         {/* Menu button on mobile devices */}
         <button
-          className="text-gray-800 sm:hidden"
+          className="text-customBlue sm:hidden"
           onClick={toggleSidebar}
         >
           {/* Menu icon */}
@@ -82,10 +90,10 @@ export default function Navbar() {
       <div
         className={`fixed top-0 left-0 w-64 h-full bg-white shadow-lg transform ${
           isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-        } transition-transform duration-300 ease-in-out sm:hidden z-50`}
+        } transition-transform duration-300 ease-in-out z-50`}
       >
         <button
-          className="text-gray-800 p-4"
+          className="text-customBlue p-4"
           onClick={toggleSidebar}
         >
           ✕
@@ -94,28 +102,28 @@ export default function Navbar() {
           <li className="list-none">
             <Link
               to="/"
-              className="flex items-center text-gray-800 hover:text-blue-600 transition-colors duration-200"
+              className="flex items-center text-customBlue hover:text-blue-600 transition-colors duration-200"
             >
               {homeIcon}
-              <span className="ml-2 text-base sm:text-lg">Inicio</span>
+              <span className="ml-2 text-base">Inicio</span>
             </Link>
           </li>
           <li className="list-none">
             <Link
               to="/nosotros"
-              className="flex items-center text-gray-800 hover:text-blue-600 transition-colors duration-200"
+              className="flex items-center text-customBlue hover:text-blue-600 transition-colors duration-200"
             >
               {aboutIcon}
-              <span className="ml-2 text-base sm:text-lg">Nosotros</span>
+              <span className="ml-2 text-base">Nosotros</span>
             </Link>
           </li>
           <li className="list-none">
             <Link
               to="/contacto"
-              className="flex items-center text-gray-800 hover:text-blue-600 transition-colors duration-200"
+              className="flex items-center text-customBlue hover:text-blue-600 transition-colors duration-200"
             >
               {contactIcon}
-              <span className="ml-2 text-base sm:text-lg">Contacto</span>
+              <span className="ml-2 text-base">Contacto</span>
             </Link>
           </li>
           <li className="list-none">
@@ -123,10 +131,10 @@ export default function Navbar() {
               href={WHATSAPP_LINK}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center text-gray-800 hover:text-blue-600 transition-colors duration-200"
+              className="flex items-center text-customBlue hover:text-blue-600 transition-colors duration-200"
             >
               {whatsappIcon}
-              <span className="ml-2 text-base sm:text-lg">Whatsapp</span>
+              <span className="ml-2 text-base">Whatsapp</span>
             </a>
           </li>
         </ul>
